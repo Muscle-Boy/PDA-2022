37406091a7b8b4dcf766e9f1974e93830a5ac592
burntpie <80338962+burntpie@users.noreply.github.com>
2022-07-14 14:58:28 +0800

    push disinformation branch

---
+++ disinformation.py
@@ -0,0 +1,95 @@
+from flask import Flask, request, jsonify
+from disinformation_modeling import *
+from datetime import datetime
+
+
+# _____________________________________________ Flask Configurations ________________________________________________
+
+IS_LOCAL_DEPLOYMENT = True
+
+app = Flask(__name__)
+
+# _____________________________________________ Text Input Routes ___________________________________________________
+
+@app.route('/text-input', methods=['POST'])
+def text_input():
+
+    try:
+        input_json = request.get_json()
+
+        input_text = input_json["input_text"]
+        trusted_sources = input_json["trusted_sources"]
+        article_count = input_json["article_count"]
+
+        article_list, search_term, result = disinformation_analysis(input_text, trusted_sources, article_count, False)
+
+        sentiment_dict = sentiment_analysis(input_text)
+
+        return_json = {
+            "input_text" : input_text,
+            "search_term" : search_term,
+            "result" : result,
+            "sentiment_label" : sentiment_dict['label'],
+            "sentiment_score" : sentiment_dict['score'],
+            "date_created": datetime.now(),
+            "Articles" : [
+                {
+                    "url" : article.url,
+                    "title" : article.title,
+                    "full_article" : article.full_article,
+                    "summarized_article" : article.summarized_article,
+                    "similarity_score" : article.similarity_score
+                } for article in article_list
+            ]
+        }
+
+        return jsonify(return_json), 200
+    except Exception as e:
+        return f"{e}", 404
+
+# _____________________________________________ Batch Processing Routes _____________________________________________
+
+@app.route('/batch-processing', methods=['POST'])
+def batch_processing():
+    
+    try:
+        input_list = request.get_json()
+        return_json = {
+            "batch_result_list" : []
+        }
+
+        for input in input_list:
+            input_text = input["input_text"]
+
+            article_list, search_term, result = disinformation_analysis(input_text, ['CNA', 'today'], 3, True)
+
+            sentiment_dict = sentiment_analysis(input_text)
+
+            return_dict = {
+                "input_text" : input_text,
+                "result" : result,
+                "sentiment_label" : sentiment_dict['label'],
+                "sentiment_score" : sentiment_dict['score'],
+                "Articles" : [
+                    {
+                        "url" : article.url,
+                        "title" : article.title,
+                    } for article in article_list
+                ]
+            }
+
+            return_json["batch_result_list"].append(return_dict)
+        
+        return_json["date_created"] = datetime.now()
+
+        return jsonify(return_json), 200
+    except Exception as e:
+        return f"{e}", 404
+
+# _____________________________________________ Helper Functions ____________________________________________________
+
+if __name__ == "__main__":
+    if IS_LOCAL_DEPLOYMENT:
+        app.run(debug=True, port=8020) # host="0.0.0.0"
+    else:
+        app.run(host="0.0.0.0", port=5020)
---
+++ disinformation_modeling.py
@@ -0,0 +1,139 @@
+from transformers import pipeline
+from sentence_transformers import SentenceTransformer, util
+from googlesearch import search
+from bs4 import BeautifulSoup
+import requests
+
+# _____________________________________________ Sentiment Analysis __________________________________________________
+
+classifier = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
+
+def sentiment_analysis(input_text):
+    result_dict = classifier(input_text)[0]
+    
+    return result_dict
+
+
+# _____________________________________________ Disinformation Analysis _____________________________________________
+
+class Article:
+    def __init__(self, url, title, full_article, summarized_article, similarity_score) -> None:
+        self.url = url
+        self.title = title
+        self.full_article = full_article
+        self.summarized_article = summarized_article
+        self.similarity_score = similarity_score
+
+
+summarizer = pipeline("summarization", model="facebook/bart-large-cnn", framework="pt")
+similarity_model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+
+def summarize(text):
+    summary = summarizer(text, max_length=80, min_length=0, do_sample=False)
+
+    return summary[0].get('summary_text')
+
+def similarity_check(text1, text2):
+    embedding_1= similarity_model.encode(text1, convert_to_tensor=True)
+    embedding_2 = similarity_model.encode(text2, convert_to_tensor=True)
+
+    res_tensor = util.pytorch_cos_sim(embedding_1, embedding_2)
+
+    return res_tensor.item()
+
+def disinformation_analysis(input_text, trusted_sources, article_count, is_batch_processing):
+    if not trusted_sources:
+        raise Exception("Error: Must include trusted sources")
+
+    similarity_list = []
+    article_list = []
+
+    if len(input_text) > 100:
+        text = summarize(input_text)
+    else:
+        text = input_text
+    
+    for source in trusted_sources:
+        if source == "CNA":
+            text += " site:channelnewsasia.com"
+        elif source == "today":
+            text += " site:todayonline.com"
+        elif source == "gov.sg":
+            text += " site:gov.sg"
+        text += " OR"
+
+    search_term = text[:-3]
+    # print(search_term)
+    url_list = search(search_term, tld="co.in", num=article_count, stop=article_count, pause=2)
+
+    headers = { 'accept':'*/*',
+        'accept-encoding':'gzip, deflate, br',
+        'accept-language':'en-GB,en;q=0.9,en-US;q=0.8,hi;q=0.7,la;q=0.6',
+        'cache-control':'no-cache',
+        'dnt':'1',
+        'pragma':'no-cache',
+        'referer':'https',
+        'sec-fetch-mode':'no-cors',
+        'sec-fetch-site':'cross-site',
+        'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
+    }
+
+    for url in url_list:
+        page = requests.get(url, headers=headers)
+        if page.status_code == 200:
+            try:
+                soup = BeautifulSoup(page.content, "lxml")
+                res_soup = soup.find('main', class_='main-content')
+                title = res_soup.article['title']
+                elements = res_soup.find_all('p')
+                full_article = ""
+                for element in elements:
+                    if element == None:
+                        continue
+                    full_article += " "
+                    full_article += element.text.strip()
+            except Exception as e:
+                title = None
+                full_article = None
+                print(f"Scraping Error! - {e}")
+
+            if not is_batch_processing:
+                try:
+                    summarized_article = summarize(full_article)
+                except Exception as e:
+                    summarized_article = None
+                    print(f"Summarizing Error! - {e}")
+            else:
+                summarized_article = None
+
+            try:
+                similarity_score = similarity_check(input_text, full_article)
+                similarity_list.append(similarity_score)
+
+                similarity_score = round(similarity_score*100, 2)
+            except Exception as e:
+                similarity_score = None
+                print(f"Similarity Comparision Error! - {e}")
+
+        else:
+            print(f"Unable to retrieve page - Error Code {page.status_code}")
+        
+        article_list.append(Article(url, title, full_article, summarized_article, similarity_score))
+
+
+    try:
+        true_score = round(sum(similarity_list) / len(similarity_list)*100, 2)
+        print(f"The Input Text is likely to be {true_score}% true.")
+
+        if true_score > 50:
+            result = f"True with {true_score}% likelihood"
+        else:
+            result = f"False with {100 - true_score}% likelihood"
+
+    except Exception as e:
+        true_score = None
+        print(f"Score Error! {e}")
+
+    return article_list, search_term, result
+        
+
